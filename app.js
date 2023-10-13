b024b4c9e96cf0bc2a71cf854617c6470b5ec951
songyoungjun <clesaaaaaa@gmail.com>
2023-10-13 10:45:45 +0900

    응답기준 url설정

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const http = require('http')
 const fs = require('fs')
-const { listenerCount } = require('process')
 http.createServer(function(req, res){
   console.log(req.url)
   let writeHeadObject = {
     'Content-Type' : 'text/html'
   }
+  if(req.url === "/"){
   res.writeHead(200, writeHeadObject)
   fs.readFile("index.html", function(err, data){
     if(err) {
       console.error('파일을 읽지 못했습니다.');
     }else {
       res.end(data);
-
-}})}).listen(8080);
+    
+}})}}).listen(8080);
