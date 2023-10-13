43ac4d9f2c64acfd5e7524f6f9a9b8b3232f4de0
songyoungjun <clesaaaaaa@gmail.com>
2023-10-13 10:31:52 +0900

    응답메시지 헤더

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const fs = require('fs')
 http.createServer(function(req, res){
+  console.log(request.url)
   let writeHeadObject = {
     'Content-Type' : 'text/html'
   }
+  res.writeHead(200, writeHeadObject)
 })
